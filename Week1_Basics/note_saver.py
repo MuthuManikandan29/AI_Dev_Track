59be5acd9145e63f5bfa852e27761d0a55297da4
wikki <muthumanikandanapk@gmail.com>
2025-07-29 23:51:52 +0530

    note_saver.py
    got into file handling + working with csv files

--- Week1_Basics/note_saver.py
+++ Week1_Basics/note_saver.py
@@ -2,3 +2,15 @@
 # Ask user to write a note and save it to a file called notes.txt.
 # Then read and print all notes saved so far.
 
+note = input("Write your note: ")
+
+with open("notes.txt", "a") as file:
+    file.write(note + "\n")
+
+print("\n✅ Note saved successfully!\n")
+
+print("📓 All Notes:")
+with open("notes.txt", "r") as file:
+    notes = file.readlines()
+    for line in notes:
+        print("- " + line.strip())
