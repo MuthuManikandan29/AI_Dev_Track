9af42112dcb86083ded1cafc8798404c07969c2d
wikki <muthumanikandanapk@gmail.com>
2025-08-06 18:11:26 +0530

    Dictionaries_sample
    Accesing elements in Dictionary

---
+++ week2_alongwithit/dictionaries_sample.py
@@ -0,0 +1,11 @@
+skills=["coding","public speaking","entrepreneur"]
+about_me={
+    "name":"wikki",
+    "age":22,
+    "skill":skills,
+    "is_job_ready":True
+}
+print(about_me["name"])
+print(skills)
+print(skills[0])
+print(about_me)
