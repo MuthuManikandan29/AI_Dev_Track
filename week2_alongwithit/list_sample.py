35f912210a891ebf037dbbcbe943ac9f6858dc50
wikki <muthumanikandanapk@gmail.com>
2025-08-06 18:09:31 +0530

    List_samples
    diving into lists

---
+++ week2_alongwithit/list_sample.py
@@ -0,0 +1,18 @@
+# doubled=[]
+# numbers = [1, 2, 3, 4]
+# for n in numbers:
+#     doubled.append(n**2)
+# print(doubled)
+
+
+# new_list=[]
+# name_count=int(input("enter how many names ? "))
+# for i in range(name_count):
+#     name=input("enter the name : ")
+#     new_list.append(name)
+#     avg = len(name) / name_count
+#     print(len(name))
+#     print(avg)
+# print(new_list)
+
+
