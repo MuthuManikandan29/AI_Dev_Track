207895da83fd8907468feafefa87eada799ff55b
wikki <muthumanikandanapk@gmail.com>
2025-08-13 11:34:31 +0530

    done things

---
+++ week2_alongwithit/more_functions.py
@@ -0,0 +1,15 @@
+# def add_numbers(a,b):
+#     return a+b
+# result=add_numbers(5,7)
+# print("the sum is :",result)
+
+
+# x = "awesome"
+#
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+#
+# myfunc()
+#
+# print("Python is " + x)
---
+++ week2_alongwithit/tuple_sample.py
@@ -0,0 +1,3 @@
+my_self=("Muthu Manikandan",22,"Python")
+print(my_self)
+print(my_self[2])
